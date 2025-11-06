e66a64946496e6750b6e9b10f2671173b80524fa
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-11-06 13:20:50 +0530

    Basic structure of node project
    This structure is contain an Controller, Utilities, .env

---
+++ Structure-Practice=Base_Setup/1/utilities/constant.js
@@ -0,0 +1,9 @@
+const constant = {
+    DB_NAME : process.env.DB_NAME,
+    Models : {
+        admin : admin,
+        user : user
+    }
+};
+
+module.exports = constant;
---
+++ Structure-Practice=Base_Setup/2/utilities/constant.js
@@ -0,0 +1,9 @@
+const constant = {
+    DB_NAME : process.env.DB_NAME,
+    Models : {
+        user : user,
+        admin : admin,
+    }
+};
+
+module.exports = constant;
