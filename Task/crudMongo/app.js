3aad4b810229bed425579a22899efbfc4f2246b6
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-11-17 14:25:33 +0530

    Update app.js

--- Task/crudMongo/app.js
+++ Task/crudMongo/app.js
@@ -11,14 +11,7 @@ const mongoose = require('mongoose');
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
-const adminRoutes = [
-  {path:"/userList", routePath:"/userList"},
-];
 
-const userRoutes = [
-  {path:"/login", routePath:"/login"},
-  {path:"/register", routePath:"/register"},
-];
 
 var app = express();
 
