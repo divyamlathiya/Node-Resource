acccb747fbacc7ed5b3ec9b999be099c3c08f68a
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-04 14:50:42 +0530

    Add authentication middleware to cart view route

--- Task/E-Commerce_coreModule/routes/cart/view.js
+++ Task/E-Commerce_coreModule/routes/cart/view.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var viewCart = require('../../controller/cart/view.js');
+var auth = require('../../middleware/auth.js');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', auth, async function(req, res, next) {
 
   await viewCart(req, res);
 
 });
 
 module.exports = router;
+
