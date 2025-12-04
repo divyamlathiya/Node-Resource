a810cbe82608bd04f3e30f9d078f9d2a9d2c7ba4
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-04 14:50:19 +0530

    Add authentication middleware to remove product route

--- Task/E-Commerce_coreModule/routes/cart/remove.js
+++ Task/E-Commerce_coreModule/routes/cart/remove.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var removeProduct = require('../../controller/cart/remove.js');
+var auth = require('../../middleware/auth.js');
 
 /* GET home page. */
-router.post('/', async function(req, res, next) {
+router.post('/', auth, async function(req, res, next) {
 
   await removeProduct(req, res);
 
 });
 
 module.exports = router;
+
