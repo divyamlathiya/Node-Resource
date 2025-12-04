94a823615c2237c60f546ae1a0340a08a928370a
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-04 14:48:40 +0530

    Enhance auth middleware with token validation errors

--- Task/E-Commerce_coreModule/middleware/auth.js
+++ Task/E-Commerce_coreModule/middleware/auth.js
@@ -3,21 +3,30 @@ var response = require('../utilities/responseManager.js');
 require('dotenv').config();
 
 module.exports = function(req, res, next) {
-const authHeader = req.headers.authorization;
+const authHeader = req.header('Authorization');
 
-if (!authHeader) {
-    response.onError(res, 'User not loggin');
-} else {
+if (authHeader) {
     const token = authHeader.split(' ')[1];
     if (token) {
+        try {    
         const decode = jwt.verify(token, process.env.JSON_SECRET);
-
+    
         req.user = decode;
         next();
+        } catch (error) {
+            if (error.name === 'TokenExpiredError') {
+                response.onError(res, 'Token is expired, please re-login');
+            }
+
+            if (error.name === 'JsonWebTokenError') {
+                response.onError(res, 'Token is not valid');
+            }
+        }
     } else {
         response.onError(response, 'Token is missing')
     }
+} else {
+    response.onError(res, 'User not loggin');
 }
 
 };
-
